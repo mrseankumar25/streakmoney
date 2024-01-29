505205f2e6a65d3025fad229a02c7318da7472b3
Sandeep Kumar <43328839+mrseankumar25@users.noreply.github.com>
2024-01-30 03:09:11 +0530

    Game Page under development

---
+++ admin/js/app.js
@@ -0,0 +1,50 @@
+if(typeof ContentTabs !== 'function') {
+  class ContentTabs extends HTMLElement {
+    constructor() {
+      super()
+
+      this.links = this.querySelectorAll('.nav-link')
+      this.tabs = this.querySelectorAll('.tab-content')
+
+      this.init()
+      if(this.links) this.links[0].click()
+    }
+
+    init() {
+      Array.from(this.links).forEach(link => {
+        link.addEventListener('click', (e) => {
+          e.preventDefault()
+
+          if(this.querySelector('.active')) this.querySelector('.active').classList.remove('active')
+          link.classList.add('active')
+        
+          if(this.querySelector('.tab-active')) this.querySelector('.tab-active').classList.remove('tab-active')
+          this.querySelector(link.dataset.target).classList.add('tab-active')
+        })
+      })
+    }
+  }
+
+  customElements.define('content-tabs', ContentTabs)
+}
+
+const btnDialogs = document.querySelectorAll('.btn-dialog')
+Array.from(btnDialogs).forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const dialog = btn.closest('form').querySelector(`#${ btn.dataset.target }`)
+    if(dialog) {
+      dialog.showModal()
+    }
+  })
+})
+const closeDialogs = document.querySelectorAll('.closeDialog')
+Array.from(closeDialogs).forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const dialog = btn.closest('dialog')
+    if(dialog) {
+      dialog.close()
+    }
+  })
+})
